5dd9e6bf038c645953cfea7654ecbc37da8ec041
NickAzn <nicholaslam16@gmail.com>
2020-04-10 20:42:24 -0700

    Project Setup

---
+++ Assets/Scripts/HandController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandController : MonoBehaviour {
+
+    int layerMask = 1 << 8;
+    public Transform raycastIndicator;
+
+    private void Update() {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 10f, layerMask)) {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.red);
+            raycastIndicator.position = hit.point;
+            Debug.Log("RaycastHit");
+        }
+    }
+}
---
+++ Assets/Scripts/MovableObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovableObject : MonoBehaviour {
+
+    bool held = false;
+
+    public void Grabbed() {
+        held = true;
+    }
+
+    public void Released() {
+        held = false;
+    }
+}
